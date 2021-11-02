32cd779a63be5fea1756f1de10fadb6db8ecde97
WAHEED ARSHAD <70683576+Waheed-Programmer@users.noreply.github.com>
2021-11-02 14:26:53 +0500

    Add view model folder and view modelClass for employe

---
+++ CRUD Asp(MVC) JQuery(Ajax)/ViewModel/ViewModelEmploye.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CRUD_Asp_MVC__JQuery_Ajax_.ViewModel
+{
+    public class ViewModelEmploye
+    {
+        public int EmployeId { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int CityId { get; set; }
+    }
+}
