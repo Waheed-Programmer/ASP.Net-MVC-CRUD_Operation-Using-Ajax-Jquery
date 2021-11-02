d4af1fbdb167ef80be77bc45540551c2e6d4bdc0
WAHEED ARSHAD <70683576+Waheed-Programmer@users.noreply.github.com>
2021-11-02 14:29:26 +0500

    Change view of index

--- CRUD Asp(MVC) JQuery(Ajax)/Controllers/HomeController.cs
+++ CRUD Asp(MVC) JQuery(Ajax)/Controllers/HomeController.cs
@@ -13,18 +13,6 @@ namespace CRUD_Asp_MVC__JQuery_Ajax_.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
+       
     }
 }
